f463f2b730589e272a367d2e7efee65b19b3ee26
npaudyal <npaudyal321@gmail.com>
2024-04-30 19:36:23 +0545

    class create

--- crud/urls.py
+++ crud/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import student
+from .views import student, classroom
 
 
 urlpatterns = [
     path('', student, name="crud_student"),
+    path('classroom/', classroom, name="crud_classroom")
 ]
--- crud/views.py
+++ crud/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
-from .models import Student
+from django.shortcuts import render, redirect
+from .models import Student, ClassRoom
 
 
 def student(request):
     students = Student.objects.all()
     return render(request, template_name="crud/student.html", context={"students": students})
+
+
+def classroom(request):
+    if request.method == "POST":
+        print(request.POST)
+        classroom_name = request.POST.get("classroom_name")
+        ClassRoom.objects.create(name=classroom_name)
+        return redirect("crud_classroom")
+    classrooms = ClassRoom.objects.all()
+    return render(request, template_name="crud/classroom.html", context={"classrooms": classrooms})
