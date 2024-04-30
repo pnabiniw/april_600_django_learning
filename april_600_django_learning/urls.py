7fdd5267d9d77ef567fde208623df0c36785fe8e
npaudyal <npaudyal321@gmail.com>
2024-04-30 19:02:36 +0545

    crud student

--- april_600_django_learning/urls.py
+++ april_600_django_learning/urls.py
@@ -5,5 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("commons/", include("commons.urls")),
+    path('crud/', include('crud.urls')),
     path("", include("myapp.urls"))  # URL Chaining
 ]
--- crud/views.py
+++ crud/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Student
 
-# Create your views here.
+
+def student(request):
+    students = Student.objects.all()
+    return render(request, template_name="crud/student.html", context={"students": students})
