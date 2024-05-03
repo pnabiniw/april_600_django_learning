18d1d8179d989ac93d8d67ccec642788e8f102cf
npaudyal <npaudyal321@gmail.com>
2024-05-03 19:30:17 +0545

    add student

--- crud/urls.py
+++ crud/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import student, classroom, classroom_update
+from .views import student, classroom, classroom_update, classroom_delete, add_student
 
 urlpatterns = [
     path("classroom/<int:id>/", classroom_update, name="crud_classroom_update"),
+    path("classroom-delete/<int:id>/", classroom_delete, name="crud_classroom_delete"),
     path('classroom/', classroom, name="crud_classroom"),
+    path('add-student/', add_student, name="crud_add_student"),
     path('', student, name="crud_student"),
 ]
--- crud/views.py
+++ crud/views.py
@@ -3,7 +3,7 @@ from .models import Student, ClassRoom
 
 
 def student(request):
-    students = Student.objects.all()
+    students = Student.objects.all().order_by('-id')
     return render(request, template_name="crud/student.html", context={"students": students})
 
 
@@ -13,7 +13,7 @@ def classroom(request):
         classroom_name = request.POST.get("classroom_name")
         ClassRoom.objects.create(name=classroom_name)
         return redirect("crud_classroom")
-    classrooms = ClassRoom.objects.all()
+    classrooms = ClassRoom.objects.all().order_by('-id')
     return render(request, template_name="crud/classroom.html", context={"classrooms": classrooms})
 
 
@@ -25,3 +25,27 @@ def classroom_update(request, id):
         c.save()
         return redirect("crud_classroom")
     return render(request, template_name="crud/classroom_update.html", context={"classroom": c})
+
+
+def classroom_delete(request, id):
+    c = ClassRoom.objects.get(id=id)
+    if request.method == "POST":
+        c.delete()
+        return redirect("crud_classroom")
+    return render(request, template_name="crud/classroom_delete.html", context={"classroom": c})
+
+
+def add_student(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        age = request.POST.get("age")
+        email = request.POST.get("email")
+        address = request.POST.get("address")
+        classroom_id = request.POST.get("classroom")
+        if classroom_id != "null":
+            Student.objects.create(name=name, age=age, email=email, address=address, classroom_id=classroom_id)
+        else:
+            Student.objects.create(name=name, age=age, email=email, address=address)
+        return redirect("crud_student")
+    classrooms = ClassRoom.objects.all()
+    return render(request, template_name="crud/add_student.html", context={"classrooms": classrooms})
