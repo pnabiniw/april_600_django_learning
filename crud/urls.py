e3405ebb9057aac1468a88c18169edeb3500ea7e
npaudyal <npaudyal321@gmail.com>
2024-05-03 09:11:49 +0545

    form

--- crud/urls.py
+++ crud/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import student, classroom
-
+from .views import student, classroom, classroom_update
 
 urlpatterns = [
+    path("classroom/<int:id>/", classroom_update, name="crud_classroom_update"),
+    path('classroom/', classroom, name="crud_classroom"),
     path('', student, name="crud_student"),
-    path('classroom/', classroom, name="crud_classroom")
 ]
--- crud/views.py
+++ crud/views.py
@@ -15,3 +15,13 @@ def classroom(request):
         return redirect("crud_classroom")
     classrooms = ClassRoom.objects.all()
     return render(request, template_name="crud/classroom.html", context={"classrooms": classrooms})
+
+
+def classroom_update(request, id):
+    c = ClassRoom.objects.get(id=id)
+    if request.method == "POST":
+        n = request.POST.get("classroom_name")
+        c.name = n
+        c.save()
+        return redirect("crud_classroom")
+    return render(request, template_name="crud/classroom_update.html", context={"classroom": c})
